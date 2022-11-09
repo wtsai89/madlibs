954f383b3599939d93709e911cd91375df3b5dca
wtsai89 <38266315+wtsai89@users.noreply.github.com>
2022-11-08 19:57:26 -0800

    add main file

---
+++ test.py
@@ -0,0 +1,5 @@
+name = input("Name: ")
+noun = input("Noun: ")
+verb = input("Verb: ")
+noun2 = input("Noun2: ")
+print(f"Hi my name is {name}. My favorite thing is {noun}. I like to {verb} {noun2}")
